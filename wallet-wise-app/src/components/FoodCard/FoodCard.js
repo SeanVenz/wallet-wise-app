81a3cbce67dadccd3d08e6a3152fef66bf475989
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-09-11 10:13:10 +0800

    COMPLETE ADD TO CART

--- wallet-wise-app/src/components/FoodCard/FoodCard.js
+++ wallet-wise-app/src/components/FoodCard/FoodCard.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodCard.css";
 import cart from "../../images/cart.png";
 import map from "../../images/location.png";
+import { db } from '../../utils/firebase';
+import { doc, setDoc } from "firebase/firestore";
+import authService from "../../service/auth";
 
 export const FoodCard = (props) => {
   const { img, name, price } = props;
+  const [showModal, setShowModal] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleAddToCart = async () => {
+    try {
+      const user = authService.getCurrentUser();
+      if (user) {
+        const userId = user.uid; // Get the user's UID
+        const itemId = `${userId}-${name}-${Date.now()}`;
+  
+        // Reference to the user-specific cart collection
+        const cartDocRef = doc(db, "carts", userId, "items", itemId);
+  
+        await setDoc(cartDocRef, {
+          name: name,
+          unitPrice: price,
+          quantity: quantity,
+          totalPrice: price * quantity
+        });
+  
+        console.log("Item added to cart with ID:", itemId);
+  
+        // Reset form fields and options
+        setQuantity(1);
+        setShowModal(false);
+      } else {
+        // Handle the case where the user is not authenticated
+        console.error("User is not authenticated.");
+      }
+    } catch (error) {
+      console.error("Error adding item to cart:", error);
+    }
+  };
 
   return (
     <div>
@@ -21,11 +65,25 @@ export const FoodCard = (props) => {
           </h4>
         </div>
         <div className="bottom">
-          <img src={cart} alt="cart" />
+          <img src={cart} alt="cart" onClick={handleOpenModal} />
           <img src={map} alt="map" />
         </div>
       </div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Add to Cart</h2>
+            <label>Quantity:</label>
+            <input
+              type="number"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+            />
+            <button onClick={handleAddToCart}>Add to Cart</button>
+            <button onClick={handleCloseModal}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
-
