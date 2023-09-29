8ed6654c16e9255ea72017ce025013b94ffe393d
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-09-29 14:07:15 +0800

    remodify

--- wallet-wise-app/src/pages/ForgotPassword/ForgotPassword.js
+++ wallet-wise-app/src/pages/ForgotPassword/ForgotPassword.js
@@ -1,70 +1,42 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import authService from "../../utils/auth";
-import "./ForgotPassword.css";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import authService from '../../utils/auth';
+import './ForgotPassword.css'
 
 const ForgotPassword = () => {
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState('');
   const [error, setError] = useState(null);
-  const [message, setMessage] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [emailSent, setEmailSent] = useState(false);
+  const [message, setMessage] = useState('');
 
   const handleForgotPassword = async (e) => {
-    setIsSubmitting(true);
     e.preventDefault();
     try {
       await authService.sendResetPasswordEmail(email);
-      setEmailSent(true);
-      setMessage("Password reset email has been sent.");
+      setMessage('Password reset email has been sent.');
     } catch (err) {
       setError(err.message);
-    } finally {
-      setIsSubmitting(false); // Ensure to reset isSubmitting when done
     }
   };
 
   return (
-    <div className="pass-body">
-      <div className="pass-potato"></div>
-      <div className="pass-smallbox">
-        <div className="pass-wallet">WALLET</div>
-        <div className="pass-wise">WISE</div>
-        {emailSent ? (
-          <>
-            <p>{message}</p>
-            <Link to="/login">
-              <button type="login-btn-forgot">Login</button>
-            </Link>
-          </>
-        ) : (
-          <>
-            <div className="pass-message">
-              {isSubmitting
-                ? "Sending email..."
-                : "We will send a verification code to the email below"}
-            </div>
-            <form onSubmit={handleForgotPassword}>
-              <input
-                type="forgot-email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {isSubmitting ? (
-                <div className="forgot-message">
-                  {message && <p>{message}</p>}
-                </div>
-              ) : (
-                <button type="forgot-submit">Send password reset email</button>
-              )}
-            </form>
-            <Link to="/login">
-              <button type="login-btn-forgot">Login</button>
-            </Link>
-          </>
-        )}
-        <div className="forgot-error-message">{error && <div className="forgot-error-message"><p>{error}</p></div>}</div>
+    <div className='pass-body'>
+      <div className='pass-potato'></div>
+      <div className='pass-smallbox'>
+        <div className='pass-wallet'>WALLET</div>
+        <div className='pass-wise'>WISE</div>
+        <div className='pass-message'>We will send a verification code to the email below</div>
+      <form onSubmit={handleForgotPassword}>
+        <input
+          type="forgot-email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {/* <div className='forgot-message'>{message && <p>{message}</p>}</div> */}
+        <div className="forgot-error-message">{error && <p>{error}</p>}</div>
+        <button type="forgot-submit">Send password reset email</button>
+      </form>
+      <Link to="/login"><button type='login-btn-forgot'>Login</button></Link>
       </div>
     </div>
   );
