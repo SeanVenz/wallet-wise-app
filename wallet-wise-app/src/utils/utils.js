bd44e9f293425322dc9e145677f4e987ed8e78a8
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-10-02 20:28:39 +0800

    add utils for mapping

---
+++ wallet-wise-app/src/utils/utils.js
@@ -0,0 +1,12 @@
+export function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const formattedDate = date.toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  return formattedDate;
+}
