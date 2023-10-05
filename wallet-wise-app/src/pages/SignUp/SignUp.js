d3250c3d7d3904a7a91a81378df2ceb845433309
Shane Denney Cuizon <shanedenney.cuizon@cit.edu>
2023-10-05 23:23:44 +0800

    make map into modal

--- wallet-wise-app/src/pages/SignUp/SignUp.js
+++ wallet-wise-app/src/pages/SignUp/SignUp.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import authService from "../../utils/auth";
 import "./SignUp.css";
 import MapboxMap from "components/Mapbox/Mapbox";
+import MapboxMarker from "components/Mapbox/MapBoxMarker";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -19,6 +20,14 @@ const SignUp = () => {
 
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+
+  const handleOpenMapModal = () => {
+    setShowMap(true);
+  };
+
+  const handleCloseMapModal = () => {
+    setShowMap(false);
+  };
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -68,95 +77,100 @@ const SignUp = () => {
 
   return (
     <div className="signup-parent">
-      <form onSubmit={handleSignUp} className="signup-form">
-        <div className="text-box">
-          <div className="text-box1">Welcome to</div>
-          <div className="text-box2">WALLET</div>
-          <div className="text-box3">WISE</div>
+      <div className="potato-box"></div>
+      <div className="sign-form">
+        <div className="signup-txtbox">
+            <div className="signup-txtbox1">Welcome to</div>
+            <div className="signup-txtbox2">WALLET</div>
+            <div className="signup-txtbox3">WISE</div>
         </div>
-        <input
-          className="signup-text"
-          type="text"
-          placeholder="Full Name"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-        {role === "student" ? (
-          <input
-            className="signup-text"
-            type="text"
-            placeholder="ID Number"
-            value={idNumber}
-            onChange={(e) => setIdNumber(e.target.value)}
-          />
-        ) : (
-          <input
-            className="signup-text"
-            type="text"
-            placeholder="Store Name"
-            value={storeName}
-            onChange={(e) => setStoreName(e.target.value)}
-          />
-        )}
-        <input
-          className="signup-text"
-          type="text"
-          placeholder="Phone Number"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-        <input
-          className="signup-text"
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          className="signup-text"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div>
-          <label>
-            Role:
-            <select value={role} onChange={(e) => setRole(e.target.value)}>
-              <option value="student">Student</option>
-              <option value="vendor">Vendor</option>
-            </select>
-          </label>
-        </div>
-        <div className="signup-submit"></div>
-        {isSubmitting ? (
-          <div className="success-message">
-            <h3>"Signing up..." </h3>
-          </div>
-        ) : (
-          <button type="signup-submit"> 
-            Sign up
-          </button>
-        )}
-        <div className="error-message">{error && <p>{error}</p>}</div>
-      </form>
-      {role === "vendor" && (
+        <div className="form-container">
+          <form onSubmit={handleSignUp} className="signup-form">
+            <div className="inputs">
+            <input
+              className="signup-text"
+              type="text"
+              placeholder="Full Name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+            {role === "student" ? (
+              <input
+                className="signup-text"
+                type="text"
+                placeholder="ID Number"
+                value={idNumber}
+                onChange={(e) => setIdNumber(e.target.value)}
+              />
+            ) : (
+              <input
+                className="signup-text"
+                type="text"
+                placeholder="Store Name"
+                value={storeName}
+                onChange={(e) => setStoreName(e.target.value)}
+              />
+            )}
+            <input
+              className="signup-text"
+              type="text"
+              placeholder="Phone Number"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+            <input
+              className="signup-text"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="signup-text"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            </div>
+            <div className="signup-role">
+              <label>
+                Role:
+                <select value={role} onChange={(e) => setRole(e.target.value)}>
+                  <option value="student">Student</option>
+                  <option value="vendor">Vendor</option>
+                </select>
+              </label>
+            </div>
+            {role === "vendor" && (
           <div>
             {/* Toggle the map visibility */}
-            <button onClickCapture={() => setShowMap(!showMap)}>
+            <button onClick={handleOpenMapModal}>
               {showMap ? "Close Map" : "Open Map"}
             </button>
             {/* Show the MapboxMap component when showMap is true */}
             {showMap && (
-              <MapboxMap
-                setLatitude={setLatitude}
-                setLongitude={setLongitude}
-                onClose={(e) => {handleMapClose(e)}}
-              />
+              <div className = "signup-modal">
+              <div className = "signup-mapModal">
+               <h2>MAP</h2>
+               <div className="signup-map-container">
+                <MapboxMarker latitude={latitude} longitude={longitude} />
+                </div>
+                <button onClick={handleCloseMapModal} className="signup-close-button">
+                  Close Map
+                  </button>
+                </div>
+              </div>
             )}
           </div>
         )}
-      <div className="potato-box"></div>
+            <div className="signup-submit">
+            {isSubmitting ? <><div className="success-message"><h3>"Signing up..." </h3></div></>: <button type="signup-submit">Sign up</button>}
+            </div>
+            <div className="error-message">{error && <p>{error}</p>}</div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
