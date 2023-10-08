786dbc01029e0db028c0a8f54457017aabfc502a
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-10-08 18:12:13 +0800

    finish no internet design

--- wallet-wise-app/src/pages/NoInternetError/NoInternetError.js
+++ wallet-wise-app/src/pages/NoInternetError/NoInternetError.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import './NoInternetError.scss'
-import gif from '../../images/no-internet.gif'
+import React from "react";
+import "./NoInternetError.scss";
+import gif from "../../images/no-internet.gif";
 
 function NoInternetError() {
-    return (
-      <div className='no-internet-error'>
+  return (
+    <div className="no-internet-error">
+      <div className="description">
         <p>No Internet Connection</p>
-        <img src={gif}></img>
+      </div>
+      <img src={gif}></img>
+      <div className="description">
         <p>Wild Connect Wifi ain't Wifi-ing</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-export default NoInternetError
+export default NoInternetError;
