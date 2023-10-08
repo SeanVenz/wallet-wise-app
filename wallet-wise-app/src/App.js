d798395b0b80cd9bdcf2b3625f7a6e627fd05dcb
kentsuma <kentstephensumalinog@gmail.com>
2023-10-08 21:33:31 +0800

    Landing Page mobile opt

--- wallet-wise-app/src/App.js
+++ wallet-wise-app/src/App.js
@@ -36,85 +36,91 @@ function App() {
   }
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/login"
-          element={
-            user && user.emailVerified ? <Navigate to="/student" /> : <Login />
-          }
-        />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="/signup"
-          element={
-            user && user.emailVerified ? <Navigate to="/login" /> : <SignUp />
-          }
-        />
-        <Route
-          path="/verify-email"
-          element={
-            user && !user.emailVerified ? (
-              <VerifyEmail />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            user && user.emailVerified ? (
-              <Dashboard />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            user && user.emailVerified ? <Admin /> : <Navigate to="/login" />
-          }
-        />
-        <Route
-          path="/vendor/*"
-          element={
-            user && user.emailVerified ? (
-              <div style={{ display: "flex" }}>
-                <VendorSidebar />
-                <Routes>
-                  <Route index={true} element={<Vendor />} />
-                </Routes>
-              </div>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/student/*"
-          element={
-            user && user.emailVerified ? (
-              <div style={{ display: "flex" }}>
-                <StudentSidebar />
-                <Routes>
-                  <Route index={true} element={<StudentMarket />} />
-                  <Route path="market" element={<StudentMarket />} />
-                  <Route path="profile" element={<StudentProfile />} />
-                  <Route path="orders" element={<StudentDelivery />} />
-                  <Route path="cart" element={<Cart />} />
-                </Routes>
-              </div>
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-      </Routes>
-      <Outlet />
-    </Router>
+    <div className="flex w-screen h-screen">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/login"
+            element={
+              user && user.emailVerified ? (
+                <Navigate to="/student" />
+              ) : (
+                <Login />
+              )
+            }
+          />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route
+            path="/signup"
+            element={
+              user && user.emailVerified ? <Navigate to="/login" /> : <SignUp />
+            }
+          />
+          <Route
+            path="/verify-email"
+            element={
+              user && !user.emailVerified ? (
+                <VerifyEmail />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              user && user.emailVerified ? (
+                <Dashboard />
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              user && user.emailVerified ? <Admin /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/vendor/*"
+            element={
+              user && user.emailVerified ? (
+                <div style={{ display: "flex" }}>
+                  <VendorSidebar />
+                  <Routes>
+                    <Route index={true} element={<Vendor />} />
+                  </Routes>
+                </div>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/student/*"
+            element={
+              user && user.emailVerified ? (
+                <div style={{ display: "flex" }}>
+                  <StudentSidebar />
+                  <Routes>
+                    <Route index={true} element={<StudentMarket />} />
+                    <Route path="market" element={<StudentMarket />} />
+                    <Route path="profile" element={<StudentProfile />} />
+                    <Route path="orders" element={<StudentDelivery />} />
+                    <Route path="cart" element={<Cart />} />
+                  </Routes>
+                </div>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+        </Routes>
+        <Outlet />
+      </Router>
+    </div>
   );
 }
 
