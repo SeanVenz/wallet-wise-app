990a59065564601a78e99959859e426395fbd947
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-10-12 08:53:31 +0800

    new backend

---
+++ wallet-wise-api/wallet-wise-api/Models/UserLocation.cs
@@ -0,0 +1,5 @@
+﻿public class UserLocation
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
---
+++ wallet-wise-api/wallet-wise-api/Service/UserLocationService.cs
@@ -0,0 +1,24 @@
+﻿using wallet_wise_api.Repository;
+
+namespace wallet_wise_api.Service
+{
+    public class UserLocationService : IUserLocationService
+    {
+        private readonly IUserLocationRepository _repository;
+
+        public UserLocationService(IUserLocationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public string GenerateMapWithPin(UserLocation location)
+        {
+            return _repository.GenerateMapWithPin(location);
+        }
+
+        public UserLocation GetUserLocation(int latitude, int longitude)
+        {
+            return _repository.GetUserLocation(latitude, longitude);
+        }
+    }
+}
