3c744fce56b7488a814b929a8b0906e0937ff6e4
Sean Venz Quijano <seanvenz.quijano@cit.edu>
2023-10-14 22:03:29 +0800

    remove word map

--- wallet-wise-app/src/components/FoodCard/FoodCard.js
+++ wallet-wise-app/src/components/FoodCard/FoodCard.js
@@ -157,7 +157,6 @@ export const FoodCard = (props) => {
       {showMapModal && (
         <div className="modal">
           <div className="mapModal justify-center bg-red-500">
-            <h2>MAP</h2>
             <div className="map-container">
               <MapboxMarker latitude={latitude} longitude={longitude} />
             </div>
